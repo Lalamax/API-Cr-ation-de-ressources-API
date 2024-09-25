738d19eb980f5a2cd54b107e93402b2e8540e62e
Lalama S <lalamas@MacBook-Air-de-Lalama.local>
2024-09-25 14:45:32 +0200

    Initial commit

---
+++ src/config.mjs
@@ -0,0 +1,12 @@
+export default {
+  development: {
+    type: 'development',
+    port: 3000,
+    mongodb: 'mongodb+srv://lalama92000:lalama92000@cluster0.0kxk1.mongodb.net/efrei'
+  },
+  production: {
+    type: 'production',
+    port: 3000,
+    mongodb: 'mongodb+srv://lalama92000:lalama92000@cluster0.0kxk1.mongodb.net/efrei'
+  }
+};
